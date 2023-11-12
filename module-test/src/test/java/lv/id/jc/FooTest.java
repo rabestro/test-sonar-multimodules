0ccf9990da5938cdc11a0762bce3011ba26511ef
Jegors Čemisovs <jegors_cemisovs@epam.com>
2023-11-12 17:45:39 +0200

    Update Java compiler version and improve test structure
    The Java compiler version in pom.xml has been updated to 17 for consistency and modernization. Additionally, explicit encoding has been set to UTF-8 to standardize source and output encoding. Also, in test files, specific import of assertions has been favored over generic import for clarity and avoid unnecessary imports. Finally, relative paths in module pom.xml files have been removed for independency.

--- module-test/src/test/java/lv/id/jc/FooTest.java
+++ module-test/src/test/java/lv/id/jc/FooTest.java
@@ -1,11 +1,12 @@
 package lv.id.jc;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 
-public class FooTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FooTest {
     @Test
-    public void testBar_BothNumbersPositive() {
+    void testBar_BothNumbersPositive() {
         Foo foo = new Foo();
         int result = foo.bar(7, 3);
         assertEquals(7, result, "Expected the value of x");
